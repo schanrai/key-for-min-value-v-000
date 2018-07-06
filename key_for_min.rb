1a551fb52ea1710bbeb35bcf8bdb1ebd21e85233
Sushi Chanrai <sushi@pathworks.co>
2018-07-06 00:56:34 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,12 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+name_hash = {:chair => 25, :table => 85, :mattress => 450}
 
+def key_for_min_value(name_hash)
+  lowest = ""
+   name_hash.find do |key, value|  
+    store << value 
+  end
+#sections.min { |a,b| a[:apr] <=> b[:apr] }
 end
