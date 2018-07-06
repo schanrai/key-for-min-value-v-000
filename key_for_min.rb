c8151e20e8263b18764ddf72295013c985a26472
Sushi Chanrai <sushi@pathworks.co>
2018-07-06 01:18:14 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
  name_hash.each do | key, value |
-   if lowest_key = nil || value < lowest_value
+   if lowest_key == nil || value < lowest_value
      lowest_value = value
      lowest_key = key
    end
