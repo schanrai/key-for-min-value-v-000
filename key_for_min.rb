b492d7d457c17b11399882e5b8fc72dccdca6b46
Sushi Chanrai <sushi@pathworks.co>
2018-07-06 01:04:26 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-name_hash = {:chair => 25, :table => 85, :mattress => 450}
+require 'pry'
 
 def key_for_min_value(name_hash)
   lowest_key = nil
@@ -10,6 +10,7 @@ def key_for_min_value(name_hash)
       if lowest_key == nil || value < lowest_value
         lowest_value = value
         lowest_key = key
+        binding.pry
       end
   end
 lowest_key
