182e518b6b0748f872f2d6ed5815e2793b57c68b
Sushi Chanrai <sushi@pathworks.co>
2018-07-06 01:03:19 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,12 @@ name_hash = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
-    name_hash.find do |key, value|  
+    name_hash.each do |key, value|
       if lowest_key == nil || value < lowest_value
-        
+        lowest_value = value
+        lowest_key = key
+      end
   end
+return key
 #sections.min { |a,b| a[:apr] <=> b[:apr] }
 end
